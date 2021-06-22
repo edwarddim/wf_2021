260e3964a6862bbb64b6a6658acaf01b68dc7551
Edward <eddieim@gmail.com>
2021-06-22 12:14:05 -0600

    W1D2 morning

---
+++ algo.js
@@ -0,0 +1,26 @@
+// VARIABLES
+var myName = "Edward Im" //STRING
+var age = '30' // STRING
+
+var myAge = 30 // NUMBERS OR INTEGERS
+var weight = 190.5 // FLOATS or DOUBLE
+var sleepy = false // BOOLEAN
+
+// ARRAYS - ORDERED DATA STRUCTURES
+var names = ["Edward", "John", "Jane"]
+
+console.log(names[4])
+
+// FOR LOOPS AND WHILE LOOPS
+
+
+for(var num = 1; num < 7; num+=2){
+    console.log(num)
+}
+
+var num = 1
+console.log(num)
+num++ // num = num + 1 OR num += 1
+console.log(num) // 2
+num++
+console.log(num) // 3
