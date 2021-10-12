e2bc67823cf75d3f2c14068893837d2083a1d6ef
Edward <eddieim@gmail.com>
2021-10-12 11:05:28 -0700

    W1D2 morning

---
+++ algos/w1d2.js
@@ -0,0 +1,50 @@
+// VARIABLES
+var variable_name
+
+variable_name = "Edward Im"
+
+
+var full_name = "John Doe"
+
+var age = 31
+var string_age = "31"
+
+age = age + 1
+console.log(age)
+
+string_age = string_age + 1
+console.log(string_age)
+
+var check = true
+var other = false
+
+var price = 30.22
+
+
+// var value = 0
+// console.log(value)
+// value = value + 1
+// console.log(value)
+// value = value + 1
+// console.log(value)
+
+// LOOPS
+// for(var index = 0; index <= 11; index++){
+//     console.log(index)
+// }
+// console.log("FOR LOOP FINISHED")
+// console.log("OUTSIDE THE LOOP: ", index)
+
+// var a = 25;
+// a = a - 13;
+// console.log(a/2);
+    
+// a = "hello";
+// console.log(a + " hello");
+
+for(var i=0; i<10; i++) {
+    console.log(i);
+    i = i + 3; 
+}
+    
+console.log("outside of the loop " + i);
