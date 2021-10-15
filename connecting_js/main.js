de41690a3c5f5a50c9618e38d7614f89db0aa235
Edward <eddieim@gmail.com>
2021-10-15 11:25:33 -0700

    W1D5 morning

---
+++ connecting_js/main.js
@@ -0,0 +1,16 @@
+console.log("hello world")
+
+
+// alert("Are you seeing this?")
+
+
+var answer = confirm("Do you want to accept all cookies????")
+
+console.log(answer)
+
+if(answer){
+    console.log("the user clicked on YES")
+}
+else{
+    console.log("the user clicked on NO")
+}
