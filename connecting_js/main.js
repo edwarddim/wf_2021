8bacc68fcf070d9d66fc12f42a50719d721f802a
Edward Im <eddieim@gmail.com>
2022-03-25 16:11:29 -0700

    W1D5 afternoon

---
+++ connecting_js/main.js
@@ -0,0 +1,20 @@
+/* 
+__     ___    _   _ ___ _     _        _          _ ____  
+\ \   / / \  | \ | |_ _| |   | |      / \        | / ___| 
+ \ \ / / _ \ |  \| || || |   | |     / _ \    _  | \___ \ 
+  \ V / ___ \| |\  || || |___| |___ / ___ \  | |_| |___) |
+   \_/_/   \_\_| \_|___|_____|_____/_/   \_\  \___/|____/ 
+*/
+// VANILLA JS METHODS
+
+// alert("Welcome to my Page!!!")
+
+
+var response = confirm("Are you sure you want to delete this?")
+
+if(response){
+    console.log("OK HOPE YOU KNOW WHAT YOU ARE DOING")
+}
+else{
+    console.log("OK DELETE HAS BEEN CANCELLED")
+}
