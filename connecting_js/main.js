44e5031b368655288f8bfd4e4adacb7b5e5df741
Edward Im <eddieim@gmail.com>
2022-03-28 10:58:48 -0700

    W2D1 morning

--- connecting_js/main.js
+++ connecting_js/main.js
@@ -10,11 +10,54 @@ __     ___    _   _ ___ _     _        _          _ ____
 // alert("Welcome to my Page!!!")
 
 
-var response = confirm("Are you sure you want to delete this?")
+// var response = confirm("Are you sure you want to delete this?")
 
-if(response){
-    console.log("OK HOPE YOU KNOW WHAT YOU ARE DOING")
+// if(response){
+//     console.log("OK HOPE YOU KNOW WHAT YOU ARE DOING")
+// }
+// else{
+//     console.log("OK DELETE HAS BEEN CANCELLED")
+// }
+
+
+/* 
+ ____   ___  __  __       ___ _   _ _____ ____   ___  
+|  _ \ / _ \|  \/  |     |_ _| \ | |_   _|  _ \ / _ \ 
+| | | | | | | |\/| |      | ||  \| | | | | |_) | | | |
+| |_| | |_| | |  | |      | || |\  | | | |  _ <| |_| |
+|____/ \___/|_|  |_|     |___|_| \_| |_| |_| \_\\___/ 
+*/
+
+
+// 1. DEFINING THE FUNCTION
+function clickHandler(element){
+    console.log("CLICK HANDLER FUNCTION")
+    console.log(element)
+    element.remove()
 }
-else{
-    console.log("OK DELETE HAS BEEN CANCELLED")
+
+
+function darkMode(){
+    // TARGETING AN HTML ELEMENT
+    var h1 = document.querySelector("#container > h1")
+    console.log(h1)
+
+    // CHANGE THE TEXT COLOR TO WHITE
+    h1.style.color = "white"
+    // CHANGE THE BACKGROUND COLOR TO BLACK
+    h1.style.backgroundColor = "black"
 }
+
+function liteMode(){
+    // TARGETING AN HTML ELEMENT
+    var h1 = document.querySelector("#container > h1")
+
+    // CHANGE THE TEXT COLOR TO BLACK
+    h1.style.color = "black"
+    // CHANGE THE BACKGROUND COLOR TO WHITE
+    h1.style.backgroundColor = "beige"
+}
+
+
+// 1. EVENT LISTENER - HTML
+// 2. EVENT HANDLER FUNCTION - JavaScript
