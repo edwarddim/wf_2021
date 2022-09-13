f06934644d094d0f97f6dca1e6b7d48a31718d67
Edward Im <eddieim@gmail.com>
2022-09-13 10:55:56 -0700

    W1D2 morning

---
+++ algos/week_1/day_2.js
@@ -0,0 +1,57 @@
+// VARIABLES
+// DECLARE A VARIABLE
+var firstName;
+
+// INITIALIZE A VALUE
+firstName = 'Edward'
+
+var age = 31
+
+var specificAge = 31.7
+
+var check = false
+// console.log()
+// ADDITION
+
+age = age + 3
+age += 3
+age++
+
+// SUBTRACTION
+age = age - 1
+age -= 1
+age--
+// MULTIPLICATION / DIVISION
+age = age / 2
+age /= 2
+
+age = age * 2
+age *= 2
+
+// MODULOUS OPERATOR
+age = 33
+// console.log(age % 3)
+
+
+
+
+
+// LOOPS
+// DRY - Don't Repeat Yourself
+
+// for
+
+//        0                      1         2  
+//  initialize a variable ; condition     ; increment
+// for(var index = 0         ;  index < 100  ; index++){
+//     // CODE BODY 3
+//     console.log(index)
+// }
+
+for(var i = 100; i >= 0; i++){
+    console.log(i)
+}
+
+// 0 -> 3 -> 2 -> 3 -> 2 -> 1
+
+// while
