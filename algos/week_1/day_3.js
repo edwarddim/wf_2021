a5a6d0340ad4bab656209039e8efc091bbc859c6
Edward Im <eddieim@gmail.com>
2022-09-14 10:50:04 -0700

    W1D3 morning

---
+++ algos/week_1/day_3.js
@@ -0,0 +1,44 @@
+// CONDITIONALS
+
+var i = 34
+
+if(i <= 100){
+    console.log("i is less 100")
+}
+else{
+    console.log("i is greater than 100")
+}
+// what is your age group
+// under 18
+// over 18 and under 21
+// over 21
+
+var age = 15
+if(age >= 21){
+    console.log("You can purchase alcohol, tobacco, and others")
+}
+else if(age >= 18){
+    console.log("buy lottery tix")
+}
+else{
+    console.log("you can get a slurpee")
+}
+
+
+
+
+if(firstName.length === 0 && firstName.isAlpha()){
+    console.log("First name is not valid")
+}
+
+if(lastName.length > 0){
+
+}
+
+if(password.length > 8){
+
+}
+
+if(password === confirmPassowrd){
+
+}
